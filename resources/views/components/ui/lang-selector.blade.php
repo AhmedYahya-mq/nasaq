ff067d4dd9388bd27992728d17aa60be125a9188
zain <112949845+AhmedYahya-mq@users.noreply.github.com>
2025-09-01 15:30:21 +0300

    Remove IndexController and update lang selector logic
    Deleted the unused IndexController.php file. Refactored the language selector component to dynamically generate the route name for locale switching, improving maintainability and route handling.

--- resources/views/components/ui/lang-selector.blade.php
+++ resources/views/components/ui/lang-selector.blade.php
@@ -6,14 +6,11 @@
         <button aria-label="تغيير اللغة"
             class="inline-flex px-[4px] items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-border bg-background/30 hover:bg-background/50 shadow-xs  dark:bg-input/30 dark:border-input dark:hover:bg-input/50 size-9">
             {{-- اللغة --}}
-           <picture>
-            <source srcset="{{ 'https://flagcdn.com/' . $languages[$locale] . '.svg' }}" type="image/svg+xml" />
-            <img
-                src="image source"
-                class="img-fluid shadow-md size-full"
-                alt="{{ __('messages.' . $locale) }}" loading="lazy" decoding="async"
-            />
-           </picture>
+            <picture>
+                <source srcset="{{ 'https://flagcdn.com/' . $languages[$locale] . '.svg' }}" type="image/svg+xml" />
+                <img src="image source" class="img-fluid shadow-md size-full" alt="{{ __('messages.' . $locale) }}"
+                    loading="lazy" decoding="async" />
+            </picture>
 
 
         </button>
@@ -21,9 +18,13 @@
 
     <div class="flex flex-col gap-0.5">
         {{-- زر لكل لغة --}}
+        @php
+            $routeName = Route::currentRouteName();
+            $routeBase = strstr($routeName, '.', false) ? substr($routeName, strpos($routeName, '.') + 1) : $routeName;
+            $routeName = 'locale.' . $routeBase;
+        @endphp
         @foreach (['ar' => 'ar', 'en' => 'en'] as $key => $lang)
-            <a href="{{ route(Route::currentRouteName(), $key) }}"
-            @class([
+            <a href="{{ route($routeName, $key) }}" @class([
                 'bg-primary/35' => $locale === $key,
                 "hover:bg-accent/20 text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4" => true,
             ])>
@@ -31,4 +32,4 @@
             </a>
         @endforeach
     </div>
-</x-dropdown-menu>
+    </x-dropdown-menu>
