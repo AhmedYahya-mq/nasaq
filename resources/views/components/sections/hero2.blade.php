4cf5a5f03a6b9d540740dd27d412db9649009317
zain <112949845+AhmedYahya-mq@users.noreply.github.com>
2025-09-21 01:24:09 +0300

    Update hero section background image handling
    Replaced hardcoded background image URL with Laravel's asset helper for dynamic path resolution in the hero2 section component.

--- resources/views/components/sections/hero2.blade.php
+++ resources/views/components/sections/hero2.blade.php
@@ -2,7 +2,8 @@
     <div
         class="relative overflow-hidden grid grid-cols-1 place-items-start place-content-center gap-4 p-4 h-[400px] not-lg:place-items-center dark:bg-primary-foreground bg-primary">
         <div
-            class="aspect-video rtl:clip-path-circle-right ltr:clip-path-circle-left bg-[url('http://127.0.0.1:8000/images/hero.svg')] bg-size-[70px] absolute end-0 inset-y-0 hidden lg:block lg:w-[450px]">
+        style="background-image: url('{{ asset('images/hero.svg') }}');"
+            class="aspect-video rtl:clip-path-circle-right ltr:clip-path-circle-left bg-size-[70px] absolute end-0 inset-y-0 hidden lg:block lg:w-[450px]">
             <img src="{{ asset('images/about.png') }}"
                 class="object-cover absolute end-0 rtl:clip-path-ellipse-right ltr:clip-path-ellipse-left"
                 style="block-size: 100%" alt="Hero Image">
