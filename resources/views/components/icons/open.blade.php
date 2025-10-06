e41c325d0d1b10a931d65d6ce700cdece19c31ca
zain <alzopyr377@gmail.com>
2025-10-06 16:51:40 +0300

    تعديل الاحداث

---
+++ resources/views/components/icons/open.blade.php
@@ -0,0 +1,5 @@
+<!-- أيقونة فتح / Open -->
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path d="M14 3a1 1 0 0 0 0 2h4.586l-9.293 9.293a1 1 0 0 0 1.414 1.414L20 6.414V11a1 1 0 1 0 2 0V3h-8z" />
+  <path d="M5 5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-5a1 1 0 1 0-2 0v5H5V7h5a1 1 0 1 0 0-2H5z" />
+</svg>
