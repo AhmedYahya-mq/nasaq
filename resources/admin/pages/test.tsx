78edcbad3be08081e3d46e1d2b56aa92b390a867
zain <112949845+AhmedYahya-mq@users.noreply.github.com>
2025-10-15 03:52:44 +0300

    Remove dashboard and test admin pages
    Deleted unused or obsolete dashboard.tsx and test.tsx files from the admin pages directory to clean up the codebase.

--- resources/admin/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "@inertiajs/react";
-
-export default function Test() {
-    return(
-        <div>
-            <Link href="/test2">Go to dirt</Link>
-        </div>
-    );
-}
